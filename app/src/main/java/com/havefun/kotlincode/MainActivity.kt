4920f6b203e4aea0339cbc350f44bb4526ba0209
zhangjunpu <zhangjunpu13@gmail.com>
2022-01-10 16:44:12 +0800

    fragment

--- app/src/main/java/com/havefun/kotlincode/MainActivity.kt
+++ app/src/main/java/com/havefun/kotlincode/MainActivity.kt
@@ -3,17 +3,28 @@ package com.havefun.kotlincode
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.havefun.kotlincode.fragment.MainFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val textView = findViewById<TextView>(R.id.textView)
-        MyLiveData.info.observe(this,{
+        MyLiveData.info.observe(this, {
             textView.text = it
         })
 
 
         MyLiveData.info.postValue("hello kt");
+
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment(), "TAG")
+            .commit()
+        supportFragmentManager.beginTransaction().add(MainFragment(), "TAG")
+            .commit()
     }
 }
